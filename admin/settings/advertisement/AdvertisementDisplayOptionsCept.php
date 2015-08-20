df5b6316d67d88f84c97ee61ccc105bac21dbc31
kotsios5 <pantazis.constantinos@gmail.com>
2015-08-20 18:27:01 +0300

    complete test

--- admin/settings/advertisement/AdvertisementDisplayOptionsCept.php
+++ admin/settings/advertisement/AdvertisementDisplayOptionsCept.php
@@ -11,6 +11,7 @@ $I->amOnPage('/oc-panel/');
 $I->see('welcome admin');
 
 
+
 // Contact Form
 $I->amOnPage('/oc-panel/Config/update/contact');
 $I->fillField('#formorm_config_value','0');
@@ -60,3 +61,195 @@ $I->see('Item updated. Please to see the changes delete the cache');
 //Read
 $I->amOnPage('/jobs/just-random-title-here.html');
 $I->dontSeeElement('a', ['href' => 'http://reoc.lo/oc-panel/auth/login#login-modal']);
+
+
+
+
+// QR Code
+$I->amOnPage('/oc-panel/Config/update/qr_code');
+$I->fillField('#formorm_config_value','1');
+$I->click('button[type="submit"]');
+$I->see('Item updated. Please to see the changes delete the cache');
+//Read
+$I->amOnPage('/jobs/just-random-title-here.html');
+$I->seeElement('img',['alt' => 'QR code']);
+
+// Back to default
+$I->amOnPage('/oc-panel/Config/update/qr_code');
+$I->fillField('#formorm_config_value','0');
+$I->click('button[type="submit"]');
+$I->see('Item updated. Please to see the changes delete the cache');
+//Read
+$I->amOnPage('/jobs/just-random-title-here.html');
+$I->dontSeeElement('img',['alt' => 'QR code']);
+
+
+
+// Google Maps in Ad
+$I->amOnPage('/oc-panel/Config/update/map');
+$I->fillField('#formorm_config_value','1');
+$I->click('button[type="submit"]');
+$I->see('Item updated. Please to see the changes delete the cache');
+$I->amOnPage('/oc-panel/Config/update/map_pub_new'); // enable google map in publish new
+$I->fillField('#formorm_config_value','1');
+$I->click('button[type="submit"]');
+$I->see('Item updated. Please to see the changes delete the cache');
+// Edit ad's address
+$I->amOnPage('/oc-panel/myads/update/1');
+$I->fillField('address','Madrid');
+$I->click('submit_btn');
+$I->see('Advertisement is updated');
+//Read
+$I->amOnPage('/jobs/some-nice-title-here.html');
+$I->seeElement('.img-responsive');
+
+// Back to default
+$I->amOnPage('/oc-panel/Config/update/map');
+$I->fillField('#formorm_config_value','0');
+$I->click('button[type="submit"]');
+$I->see('Item updated. Please to see the changes delete the cache');
+$I->amOnPage('/oc-panel/Config/update/map_pub_new'); // enable google map in publish new
+$I->fillField('#formorm_config_value','0');
+$I->click('button[type="submit"]');
+$I->see('Item updated. Please to see the changes delete the cache');
+// Edit ad's address
+$I->amOnPage('/oc-panel/myads/update/1');
+$I->fillField('address','optional address');
+$I->click('submit_btn');
+$I->see('Advertisement is updated');
+//Read
+$I->amOnPage('/jobs/some-nice-title-here.html');
+$I->dontSeeElement('.img-responsive');
+
+
+
+// Count Visits Ads
+$I->amOnPage('/oc-panel/Config/update/count_visits');
+$I->fillField('#formorm_config_value','0');
+$I->click('button[type="submit"]');
+$I->see('Item updated. Please to see the changes delete the cache');
+
+//Read
+$I->amOnPage('/jobs/some-nice-title-here.html');
+$I->dontSee('Hits','span');
+
+// Back to default
+$I->amOnPage('/oc-panel/Config/update/count_visits');
+$I->fillField('#formorm_config_value','1');
+$I->click('button[type="submit"]');
+$I->see('Item updated. Please to see the changes delete the cache');
+
+//Read
+$I->amOnPage('/jobs/some-nice-title-here.html');
+$I->see('Hits','span');
+
+
+
+// Show sharing buttons
+$I->amOnPage('/oc-panel/Config/update/sharing');
+$I->fillField('#formorm_config_value','1');
+$I->click('button[type="submit"]');
+$I->see('Item updated. Please to see the changes delete the cache');
+
+//Read
+$I->amOnPage('/jobs/some-nice-title-here.html');
+$I->seeElement('.st_facebook_large');
+$I->seeElement('.st_twitter_large');
+$I->seeElement('.st_linkedin_large');
+$I->seeElement('.st_pinterest_large');
+$I->seeElement('.st_googleplus_large');
+$I->seeElement('.st_email_large');
+$I->seeElement('.st_print_large');
+
+// Back to default
+$I->amOnPage('/oc-panel/Config/update/sharing');
+$I->fillField('#formorm_config_value','0');
+$I->click('button[type="submit"]');
+$I->see('Item updated. Please to see the changes delete the cache');
+
+//Read
+$I->amOnPage('/jobs/some-nice-title-here.html');
+$I->dontSeeElement('.st_facebook_large');
+$I->dontSeeElement('.st_twitter_large');
+$I->dontSeeElement('.st_linkedin_large');
+$I->dontSeeElement('.st_pinterest_large');
+$I->dontSeeElement('.st_googleplus_large');
+$I->dontSeeElement('.st_email_large');
+$I->dontSeeElement('.st_print_large');
+
+
+
+// Related Ads
+$I->amOnPage('/oc-panel/Config/update/related');
+$I->fillField('#formorm_config_value','0');
+$I->click('button[type="submit"]');
+$I->see('Item updated. Please to see the changes delete the cache');
+
+//Read
+$I->amOnPage('/jobs/some-nice-title-here.html');
+$I->dontSee('title for the ad');
+$I->dontSee('just random title here');
+$I->dontSee('Related ads','h3');
+
+// Back to default
+$I->amOnPage('/oc-panel/Config/update/related');
+$I->fillField('#formorm_config_value','5');
+$I->click('button[type="submit"]');
+$I->see('Item updated. Please to see the changes delete the cache');
+
+//Read
+$I->amOnPage('/jobs/some-nice-title-here.html');
+$I->see('title for the ad');
+$I->see('just random title here');
+$I->see('Related ads','h3');
+
+
+
+// Facebook Comments
+$I->amOnPage('/oc-panel/Config/update/fbcomments');
+$I->fillField('#formorm_config_value','367576600118660');
+$I->click('button[type="submit"]');
+$I->see('Item updated. Please to see the changes delete the cache');
+$I->amOnPage('/oc-panel/tools/cache?force=1'); // Delete cache ALL
+$I->see('All cache deleted');
+
+//Read
+$I->amOnPage('/jobs/some-nice-title-here.html');
+$I->see("//connect.facebook.net/en_US/all.js#xfbml=1&appId=367576600118660");
+
+// Back to default
+$I->amOnPage('/oc-panel/Config/update/fbcomments');
+$I->fillField('#formorm_config_value','');
+$I->click('button[type="submit"]');
+$I->see('Item updated. Please to see the changes delete the cache');
+$I->amOnPage('/oc-panel/tools/cache?force=1'); // Delete cache ALL
+$I->see('All cache deleted');
+//Read
+$I->amOnPage('/jobs/some-nice-title-here.html');
+$I->dontSee("//connect.facebook.net/en_US/all.js#xfbml=1&appId=367576600118660");
+
+
+
+// Disqus
+$I->amOnPage('/oc-panel/Config/update/disqus');
+$I->fillField('#formorm_config_value','testoc');
+$I->click('button[type="submit"]');
+$I->see('Item updated. Please to see the changes delete the cache');
+$I->amOnPage('/oc-panel/tools/cache?force=1'); // Delete cache ALL
+$I->see('All cache deleted');
+
+//Read
+$I->amOnPage('/jobs/some-nice-title-here.html');
+$I->see("comments powered by Disqus");
+
+// Back to default
+$I->amOnPage('/oc-panel/Config/update/disqus');
+$I->fillField('#formorm_config_value','');
+$I->click('button[type="submit"]');
+$I->see('Item updated. Please to see the changes delete the cache');
+$I->amOnPage('/oc-panel/tools/cache?force=1'); // Delete cache ALL
+$I->see('All cache deleted');
+//Read
+$I->amOnPage('/jobs/some-nice-title-here.html');
+$I->dontSee("comments powered by Disqus");
+
